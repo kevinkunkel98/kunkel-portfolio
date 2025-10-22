7c5a72c4ea0716701e0467bae5feffbb921c839a
kunkelmaster3000 <mm21lugi@uni-leipzig.de>
2025-10-23 01:22:40 +0200

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kevinkunkel.dev',
   output: 'static',
   adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
